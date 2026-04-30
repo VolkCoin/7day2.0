b673948eb047d2bc141fc64afc9aa03c615db270
VolkCoin <mister.volk01@gmail.com>
2026-05-01 00:11:00 +0300

    Update HeroSection.tsx

--- client/components/HeroSection.tsx
+++ client/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 export default function HeroSection() {
   return (
-    <section className="relative w-full overflow-hidden min-h-[600px] md:min-h-[750px] lg:min-h-[850px]">
+    <section className="relative w-full overflow-x-hidden min-h-[600px] md:min-h-[750px] lg:min-h-[850px]">
       {/* Green glow - top left */}
       <div
         className="absolute -top-40 -left-40 w-[500px] h-[500px] rounded-full pointer-events-none"
@@ -12,7 +12,7 @@ export default function HeroSection() {
 
       {/* Content container */}
       <div className="relative max-w-[1440px] mx-auto px-8 md:px-16 flex flex-col lg:flex-row items-center lg:items-start gap-8 pt-6 pb-16">
-        
+
         {/* Left: Text */}
         <div className="flex-1 flex flex-col gap-4 lg:gap-6 z-10 min-w-0">
           <h1
@@ -30,7 +30,7 @@ export default function HeroSection() {
         </div>
 
         {/* Right: Image */}
-        <div className="relative flex-shrink-0 w-full max-w-[340px] md:max-w-[480px] lg:max-w-[600px] xl:max-w-[700px] flex items-center justify-center">
+        <div className="relative flex-shrink-0 w-full max-w-[280px] md:max-w-[380px] lg:max-w-[480px] xl:max-w-[560px] flex items-center justify-center mr-4 lg:mr-8">
           <div
             className="absolute bottom-0 left-0 right-0 h-40 pointer-events-none z-10"
             style={{
@@ -44,6 +44,7 @@ export default function HeroSection() {
             style={{ transform: "rotate(3.4deg)" }}
           />
         </div>
+
       </div>
     </section>
   );
