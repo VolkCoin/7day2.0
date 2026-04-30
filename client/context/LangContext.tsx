103c7247438dcb540ab6c96fbea626297e48abfe
VolkCoin <mister.volk01@gmail.com>
2026-05-01 02:19:44 +0300

    Update LangContext.tsx

--- client/context/LangContext.tsx
+++ client/context/LangContext.tsx
@@ -1,1 +1,19 @@
+import { createContext, useContext, useState } from "react";
 
+export type Language = "EN" | "RU";
+
+export const LangContext = createContext<{
+  lang: Language;
+  setLang: (l: Language) => void;
+}>({ lang: "EN", setLang: () => {} });
+
+export const useLang = () => useContext(LangContext);
+
+export function LangProvider({ children }: { children: React.ReactNode }) {
+  const [lang, setLang] = useState<Language>("EN");
+  return (
+    <LangContext.Provider value={{ lang, setLang }}>
+      {children}
+    </LangContext.Provider>
+  );
+}
