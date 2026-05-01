12bf7f8e14506de58e735f8a23d66287121c8e27
VolkCoin <mister.volk01@gmail.com>
2026-05-01 20:17:59 +0300

    Update Index.tsx

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -71,3 +71,18 @@ export default function Index() {
     </LangProvider>
   );
 }
+
+<div className="fixed top-3 right-0 z-30 md:z-50">
+  <a
+    href="https://t.me/seven_day_rates"
+    className="flex items-center gap-2 bg-white pl-2 pr-4 md:pl-4 md:pr-12 py-1.5 md:py-2 shadow-xl"
+    style={{ borderRadius: "50px 0 0 50px" }}
+  >
+    <div className="scale-75 md:scale-100 origin-right">
+      <TelegramIcon />
+    </div>
+    <span className="hidden md:block font-montserrat font-semibold text-[#0A170B] text-sm">
+      Telegram
+    </span>
+  </a>
+</div>
